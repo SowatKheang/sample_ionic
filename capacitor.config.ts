bbd4adf74e33c1bd7f283551ca28b64b6b9a8929
KheangSowat <sowat.kheang@poscarasia.com>
2022-02-25 14:06:51 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sample-state-management',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
