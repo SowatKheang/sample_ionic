2a65915fef3299bc51ddd24ec8a5c46fff3a46f5
KheangSowat <sowat.kheang@poscarasia.com>
2022-06-01 10:49:28 +0700

    add action select category, size, color, and brand

--- src/app/pages/product/product.page.ts
+++ src/app/pages/product/product.page.ts
@@ -7,16 +7,29 @@ import { ProductInfo } from '../../ngxs/product/state';
 import { Product } from '../../ngxs/product/action';
 import { CategoryListModel } from '../../ngxs/category/state';
 import { Category } from '../../ngxs/category/action';
+import { PreLoad } from 'src/app/decorators/preload';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
   styleUrls: ['./product.page.scss'],
 })
+@PreLoad('ProductDetail')
 export class ProductPage extends AbstractPage {
 
+  // Static
   categories = [];
   products = [];
+  brands = [];
+  colors = [];
+  sizes = [];
+
+  selectedBrand = null;
+  selectedColor = null;
+  selectedCategory = null;
+  selectedSize = null;
+
+  // From API
   productList: ProductInfo;
   categoryList: CategoryListModel;
 
@@ -42,6 +55,45 @@ export class ProductPage extends AbstractPage {
   onInit(): void {
     this.setTitle('product');
 
+    this.colors = [
+        { desc: 'Black', class: 'color-black'},
+        { desc: 'Red', class: 'color-red'},
+        { desc: 'Yellow', class: 'color-yellow'},
+        { desc: 'White', class: 'color-white'},
+        { desc: 'Blue', class: 'color-blue'},
+        { desc: 'Green', class: 'color-green'},
+        { desc: 'Yellow Green', class: 'color-yellowgreen'},
+        { desc: 'Pink', class: 'color-pink'},
+        { desc: 'Violet', class: 'color-violet'},
+        { desc: 'Blue Violet', class: 'color-blueviolet'},
+        { desc: 'Lime', class: 'color-lime'},
+        { desc: 'Plum', class: 'color-plum'},
+        { desc: 'Teal', class: 'color-teal'},
+    ];
+
+    this.brands = [
+        { desc: 'Gucci', descEn: 'Gucci'},
+        { desc: 'Virgil Abloh', descEn: 'Virgil Abloh'},
+        { desc: 'Balenciaga', descEn: 'Balenciaga'},
+        { desc: 'Moncler', descEn: 'Moncler'},
+        { desc: 'Fendi', descEn: 'Fendi'},
+        { desc: 'Versace', descEn: 'Versace'},
+    ];
+
+    this.sizes = [
+      { desc: '20', descEn: '20'},
+      { desc: '24', descEn: '24'},
+      { desc: '29', descEn: '29'},
+      { desc: '30', descEn: '30'},
+      { desc: '36', descEn: '36'},
+      { desc: 'XS', descEn: 'XS'},
+      { desc: 'S', descEn: 'S'},
+      { desc: 'M', descEn: 'M'},
+      { desc: 'L', descEn: 'L'},
+      { desc: 'XL', descEn: 'XL'},
+      { desc: '2XL', descEn: '2XL'},
+  ];
+
     // this.categories = [
     //   { desc: 'Men\'s', descEn: 'Men\'s'},
     //   { desc: 'Women\'s', descEn: 'Women\'s'},
@@ -60,6 +112,22 @@ export class ProductPage extends AbstractPage {
     // this.getProduct();
     // this.getCategory();
 
+  }
+
+  setSelectedBrand(brand){
+    this.selectedBrand = brand;
+  }
+
+  setSelectedColor(color){
+    this.selectedColor = color;
+  }
+
+  setSelectedCategory(category){
+    this.selectedCategory = category;
+  }
+
+  setSelectedSize(size){
+    this.selectedSize = size;
   }
 
   async getCategory() {
